e7bd5ea980e6c062545c1b560d3ceffd983d4e31
Christian Beckham <urenacb22@gmail.com>
2023-01-27 15:19:17 -0600

    Initial commit

---
+++ src/test/java/com/christianbeckham/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christianbeckham.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
