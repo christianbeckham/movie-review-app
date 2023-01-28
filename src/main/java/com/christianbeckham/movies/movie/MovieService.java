99fa165212c1a900c3e281c232e459b49c7ab8e1
Christian Beckham <urenacb22@gmail.com>
2023-01-28 15:09:27 -0600

    feat: created new method to retrieve movie by id

--- src/main/java/com/christianbeckham/movies/movie/MovieService.java
+++ src/main/java/com/christianbeckham/movies/movie/MovieService.java
@@ -1,9 +1,11 @@
 package com.christianbeckham.movies.movie;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,9 @@ public class MovieService {
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
