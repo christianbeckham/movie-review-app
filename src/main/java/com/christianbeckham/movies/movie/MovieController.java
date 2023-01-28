5d79892adab435b19da77480319b37f1b442e6dc
Christian Beckham <urenacb22@gmail.com>
2023-01-28 14:33:53 -0600

    feat: created controller class with API endpoint to retrieve all movies

---
+++ src/main/java/com/christianbeckham/movies/movie/MovieController.java
@@ -0,0 +1,24 @@
+package com.christianbeckham.movies.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> allMovies = movieService.getAllMovies();
+        return new ResponseEntity<List<Movie>>(allMovies, HttpStatus.OK);
+    }
+
+}
