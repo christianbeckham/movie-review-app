6986c60dd95199ac0ed6c9aeb45c82f6ab7e49ef
Christian Beckham <urenacb22@gmail.com>
2023-01-30 11:06:19 -0600

    feat: created component for icon and title

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+import { VideoCameraTwoTone } from "@ant-design/icons";
+
+const Logo = () => {
+	return (
+		<>
+			<div
+				style={{
+					fontSize: "32px",
+				}}
+			>
+				<VideoCameraTwoTone />
+			</div>
+			<h1 style={{ color: "#fff", padding: "0 8px" }}>MovieBook</h1>
+		</>
+	);
+};
+
+export default Logo;
