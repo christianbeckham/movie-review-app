93a12088d2575947390e41dfebf3b8b7df606a44
Christian Beckham <urenacb22@gmail.com>
2023-01-30 11:12:02 -0600

    feat: implemented routing and added API request to retrieve movies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import api from "./api/axios.config";
+
+import PageLayout from "./components/PageLayout/PageLayout";
+import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
+	const [movies, setMovies] = useState([]);
+
+	const getMovies = async () => {
+		try {
+			const response = await api.get("/api/v1/movies");
+			setMovies(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getMovies();
+	}, []);
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<Routes>
+				<Route path="/" element={<PageLayout />}>
+					<Route path="/" element={<HomePage movies={movies} />} />
+				</Route>
+			</Routes>
 		</div>
 	);
 };
